0a9d8b4e0056120ab0659fe6adf241eb6563151d
tekendev01 <tekendev@gmail.com>
2025-09-08 17:54:50 -0300

    Opção de saída
    Adicionado no sistema a opção de saída para o usuário

--- cartório.c
+++ cartório.c
@@ -124,7 +124,8 @@ int main()
   	    printf("escolha a opção desejada no menu:\n\n");
 	    printf("\t1 -Registrar nomes\n");
 	    printf("\t2 -Consultar nomes\n");
-	    printf("\t3 -Deletar nomes\n\n");
+	    printf("\t3 -Deletar nomes\n");
+	    printf("\t4 -Sair do sistema\n\n");
 	    printf("opção:"); //fim do menu
 	
 	    scanf("%d", &opcao);  //armazenando escolha do usuario
@@ -144,6 +145,11 @@ int main()
 		    
 		    case 3:
 		    deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
